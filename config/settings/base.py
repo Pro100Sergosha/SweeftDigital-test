bfc645609d68c7a9a104191a53346b34826f8dc0
Sergo Azizbekyan <139613549+Pro100Sergosha@users.noreply.github.com>
2025-10-01 11:20:35 +0400

    Feature: implement authentication  (#2)
    * Feature: implement user authentication
    
    * Tests(authentication): implement pytests for authentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+    "apps.authentication",
 ]
 
 MIDDLEWARE = [
